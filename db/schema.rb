112203e458c14afb80eea7902288814261c99fa6
John Schmid <jschmid808@gmail.com>
2019-02-05 22:10:08 -0500

    Added Article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_154208) do
+ActiveRecord::Schema.define(version: 2019_02_06_030303) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
