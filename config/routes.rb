d60a0d1f0a43c1565bde47ed14ff4f5eade9860d
John Schmid <jschmid808@gmail.com>
2019-02-08 15:38:57 -0500

    Commontator init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Commontator::Engine => '/commontator'
 
   root 'pages#index'
-
   get 'music' => 'pages#music'
   get 'events' => 'pages#events'
   get 'dev' => 'pages#dev'
