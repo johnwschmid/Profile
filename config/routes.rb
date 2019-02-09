51a2c52c188588f47edb0bad0fe774d9b0e143ac
John Schmid <jschmid808@gmail.com>
2019-02-08 19:29:02 -0500

    Created articles#show (controller, action, routing)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'articles/show'
   devise_for :users
   mount Commontator::Engine => '/commontator'
 
   root 'pages#index'
-  get 'music' => 'pages#music'
-  get 'events' => 'pages#events'
-  get 'dev' => 'pages#dev'
+
+  get 'music', to: 'pages#music'
+  get 'events', to: 'pages#events'
+  get 'dev', to: 'pages#dev'
+  
 end
